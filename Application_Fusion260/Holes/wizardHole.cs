ac3396743e7ef759507d1a4d68c6015ea5c7e1dc
Martin Chassard <martin.chassard.1@ens.etsmtl.ca>
2022-11-27 13:21:59 -0500

    conservation de la version de corentin

--- Application_Fusion260/Holes/wizardHole.cs
+++ Application_Fusion260/Holes/wizardHole.cs
@@ -8,21 +8,15 @@ using System.Threading.Tasks;
 
 namespace hole_namespace
 {
-    internal class WizardHole : Hole
+    internal class wizardHole : Hole
     {
         /*
          * Constructor
          */
-        public WizardHole(double depth, double radius, int id, string functionHoleCreation, List<Face2> holeFaces) 
+        public wizardHole(double depth, double radius, int id, string functionHoleCreation, List<Face2> holeFaces) 
         { 
             this.depth = depth;
             this.radius = radius;
-            this.id = id;
-            this.functionHoleCreation = functionHoleCreation;
-            this.holeFaces = holeFaces;
-        }
-        public WizardHole(int id, string functionHoleCreation, List<Face2> holeFaces)
-        {
             this.id = id;
             this.functionHoleCreation = functionHoleCreation;
             this.holeFaces = holeFaces;
