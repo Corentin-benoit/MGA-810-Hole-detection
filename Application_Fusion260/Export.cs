175a22cff32f8d808ec035f6910017162f6987d0
Corentin-benoit <83649069+Corentin-benoit@users.noreply.github.com>
2022-12-01 15:02:40 -0500

    test export

--- Application_Fusion260/Export.cs
+++ Application_Fusion260/Export.cs
@@ -2,7 +2,20 @@
  * Using for export our holes' features
  */
 
+/*
+ * https://www.nuget.org/packages/CsvHelper/
+ * To use CSV helper, copy and paste the following line in Package Manager console (Tools -> Nugets Package Manager)
+ * NuGet\Install-Package CsvHelper -Version 30.0.1
+ * https://www.codingame.com/playgrounds/5197/writing-csv-files-using-c
+ * https://dev.to/bristolsamo/c-csv-parser-step-by-step-tutorial-25ok
+ */
+
+using System.Globalization;
+
+using CsvHelper;
+using CsvHelper.Configuration;
 using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,5 +25,6 @@ namespace Application_Fusion260
 {
     internal class Export
     {
+
     }
 }
